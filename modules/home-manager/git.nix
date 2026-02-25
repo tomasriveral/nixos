40873ea843902481225e6ea318499bebafe8f4cc
Tomas Rivera <137088692+Totorile1@users.noreply.github.com>
2026-02-25 10:48:40 +0100

    Test

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -3,11 +3,13 @@
 {
   programs.git = {
     enable = true;
+    # user.email dont work #user.email = "137088692+Totorile1@users.noreply.github.com";
+    #user.name = "Totorile1";
     # no declarative parameters for git as it would show in a public git repo more info than I would like. Just use the following commands
     # git config --global user.name "YourName"
     # git config --global user.email "YourEmail"
     # git config --global init.defaultBranch main
-
+# git config --global user.email "137088692+Totorile1@users.noreply.github.com"
 #cd ~/nixos\
 #git add . 
 #git commit -m "Update NixOS configuration" 
