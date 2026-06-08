75002217f342874c4be784afe22651e09434906b
Tomás Rivera <tomas.riveral@icloud.com>
2026-06-08 22:12:43 +0200

    fix: home.file

--- modules/utilities/nixGitCherryPicker.nix
+++ modules/utilities/nixGitCherryPicker.nix
@@ -15,4 +15,20 @@
       force = true;
     };
   };
+  flake.homeModules.nix-git-cherry-picker-desktop = {pkgs, ...}: {
+    home.packages = [
+      inputs.nix-git-cherry-picker.packages.${pkgs.system}.default
+    ];
+  };
+  home.file.".config/nix-git-cherry-picker/config.json" = {
+    enable = true;
+    text = ''
+      {
+        "localBranch": "desktop",
+        "remoteBranch": "laptop",
+        "nixConfigPath": "/home/tomasr/nixos/"
+      }
+    '';
+    force = true;
+  };
 }
