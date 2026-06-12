d49c7ba99e0296aa7b8b283f724e212014c359eb
Tomás Rivera <tomas.riveral@icloud.com>
2026-06-12 15:58:17 +0200

    limine: rice

--- modules/hardware/bootloader.nix
+++ modules/hardware/bootloader.nix
@@ -1,9 +1,11 @@
-_: {
-  flake.nixosModules.bootloader = {
-    inputs,
-    pkgs,
-    ...
-  }: {
+_: let
+  wallpaper1 = ../../assets/wallpaper1.jpg;
+  wallpaper2 = ../../assets/wallpaper2.jpg;
+  wallpaper3 = ../../assets/wallpaper3.jpg;
+  wallpaper4 = ../../assets/wallpaper4.jpg;
+  wallpaper5 = ../../assets/wallpaper5.jpg;
+in {
+  flake.nixosModules.bootloader = _: {
     # disable other bootloaders
     boot.loader.systemd-boot.enable = false;
     boot.loader.grub.enable = false;
@@ -11,8 +13,17 @@ _: {
     boot.loader.limine = {
       enable = true;
       efiSupport = true;
-      extraConfig = ''
-      '';
+      maxGenerations = 128;
+      style = {
+        wallpaperStyle = "centered";
+        wallpapers = [
+          wallpaper1
+          wallpaper2
+          wallpaper3
+          wallpaper4
+          wallpaper5
+        ];
+      };
     };
 
     boot.loader.efi.canTouchEfiVariables = true;
