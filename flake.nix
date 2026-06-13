37f76a4439ef87a5368babb5efe24c94a3ca193b
Tomás Rivera <tomas.riveral@icloud.com>
2026-06-13 09:37:01 +0200

    nixpkgs-master: follow unstable when unused

--- flake.nix
+++ flake.nix
@@ -4,7 +4,10 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable"; # used for some packages
-    nixpkgs-master.url = "github:nixos/nixpkgs/master"; # used with precaution as they are untested and not cached
+    
+    # Only uncomment when needed. This uses a lot of space and compute ressources for sometimes not that much. If not used, make it follow unstable
+    nixpkgs-master.follows = "nixpkgs-unstable";
+    #nixpkgs-master.url = "github:nixos/nixpkgs/master"; # used with precaution as they are untested and not cached
 
     # This one is only used when testing some packaging. You must change the path to the correct nixpkgs clone
     #nixpkgs-local.url = "path:/home/tomasr/devel/fugit2-gpgme";
