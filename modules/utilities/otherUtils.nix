b76090455786725896285b526149897963dc2bf9
Tomás Rivera <tomas.riveral@icloud.com>
2026-08-23 13:38:06 +0200

    bitwarden-client updated electron

--- modules/utilities/otherUtils.nix
+++ modules/utilities/otherUtils.nix
@@ -1,7 +1,7 @@
 {self, ...}: {
   flake.nixosModules.otherUtils =
     # try to keep packages here at a minium. Preferably use a dedicated file
-    {pkgs, ...}: {
+    {pkgs, pkgs-unstable, ...}: {
       environment.systemPackages = with pkgs; [
         gnome-calculator
         snapshot
@@ -18,8 +18,7 @@
         usbutils # used for lsusb
         self.packages.${pkgs.system}.custom-tomato
         self.packages.${pkgs.system}.custom-syllabes
-        # Electron 39 EOL. https://github.com/bitwarden/clients/pull/20448 should move it to 41. For the moment use the browser extension
-        #pkgs-unstable.bitwarden-desktop
+        pkgs-unstable.bitwarden-desktop
         fluffychat # matrix client
       ];
     };
