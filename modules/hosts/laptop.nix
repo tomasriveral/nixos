c2f733d4c9f1fc703f00d48e4f3a3d0caee6077c
Tomás Rivera <tomas.riveral@icloud.com>
2026-09-06 11:17:17 +0200

    setup for cs-119l

--- modules/hosts/laptop.nix
+++ modules/hosts/laptop.nix
@@ -23,6 +23,7 @@
       bootloader
       browsers
       caelestia
+      cs-119l # I'm obliged to use vscode. RIP neovim
       development
       documentation
       disk
--- modules/utilities/epfl.nix
+++ modules/utilities/epfl.nix
@@ -4,4 +4,42 @@ _: {
       openconnect
     ];
   };
+
+  flake.nixosModules.cs-119l = {pkgs, ...}: 
+  let
+    user = "YOUR_USERNAME";
+    home = "/home/${user}";
+    src = "${home}/epfl/cs-119l-ICC/";
+    dst = "${home}/kdrive/Travaille/2 - Bachelor/Information, calcul, communication CS-119l/";
+  in
+  {
+    environment.systemPackages = with pkgs; [
+      vscode-fhs
+    ];
+    systemd.services.sync-cs119l-to-kdrive = {
+      description = "Sync CS-119l ICC to kDrive";
+  
+      serviceConfig = {
+        Type = "oneshot";
+        User = user;
+  
+        ExecStart =
+          "${pkgs.rsync}/bin/rsync -a '${src}' '${dst}'";
+      };
+    };
+  
+    systemd.timers.sync-cs119l-to-kdrive = {
+      description = "Sync CS-119l ICC to kDrive every Thursday and Friday evening";
+  
+      wantedBy = [ "timers.target" ];
+  
+      timerConfig = {
+        OnCalendar = [
+          "Thu *-*-* 20:00:00"
+          "Fri *-*-* 20:00:00"
+        ];
+        Persistent = true;
+      };
+    };
+  };
 }
